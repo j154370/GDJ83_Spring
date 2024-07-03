eb467611c1980bb861b77df2cf366329ca09c390
j154370 <lihyo15@gmail.com>
2024-07-03 17:12:32 +0900

    aa

--- src/main/java/com/goodee/app/product/ProductController.java
+++ src/main/java/com/goodee/app/product/ProductController.java
@@ -39,8 +39,8 @@ public class ProductController {
 		
 	}
 	
-	@RequestMapping("add")
-	public void add() {
+	@RequestMapping(value = "add", method = RequestMethod.GET)
+	public void add() throws Exception{
 	}
 	
 	@RequestMapping(value = "add", method = RequestMethod.POST)
@@ -74,13 +74,22 @@ public class ProductController {
 		return path;
 	}
 	
-	@RequestMapping("update")
-	public void update() throws Exception{
+	@RequestMapping(value = "update", method = RequestMethod.GET)
+	public void update(Model model, ProductDTO dto) throws Exception{
+		ProductDTO dto2 = service.getDetail(dto);
 		
+		String path = "commons/message";
+		if(dto2 != null) {
+			model.addAttribute("dto", dto2);
+			path = "product/update";
+		}else {
+			model.addAttribute("result", "데이터 삭제에 실패하였습니다.");
+			model.addAttribute("url", "/product/list");
+		}
 	}
 	
 	@RequestMapping(value = "update", method = RequestMethod.POST)
-	public String update(Model model, ProductDTO dto) throws Exception{
+	public String update(ProductDTO dto) throws Exception{
 		int result = service.update(dto);
 		
 		return "redirect:/product/list";
