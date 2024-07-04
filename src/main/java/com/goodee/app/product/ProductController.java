4abd0ae917ca13386fd9551039338435ec306989
j154370 <lihyo15@gmail.com>
2024-07-04 09:01:44 +0900

    aa

--- src/main/java/com/goodee/app/product/ProductController.java
+++ src/main/java/com/goodee/app/product/ProductController.java
@@ -11,45 +11,45 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/product/*")
 public class ProductController {
-	
+
 	@Autowired
 	private ProductService service;
-	
+
 	@RequestMapping("list")
-	public void getList(Model model) throws Exception{
+	public void getList(Model model) throws Exception {
 		List<ProductDTO> dtos = service.getList();
-		
+
 		model.addAttribute("list", dtos);
 	}
-	
+
 	@RequestMapping("detail")
-	public String getDetail(Model model, ProductDTO dto) throws Exception{
+	public String getDetail(Model model, ProductDTO dto) throws Exception {
 		ProductDTO dto2 = service.getDetail(dto);
-		
+
 		String path = "commons/message";
-		if(dto2 != null) {
+		if (dto2 != null) {
 			model.addAttribute("dto", dto2);
 			path = "product/detail";
-		}else {
+		} else {
 			model.addAttribute("result", "해당 데이터를 찾을 수 없습니다.");
 			model.addAttribute("url", "/product/list");
 		}
-		
+
 		return path;
-		
+
 	}
-	
+
 	@RequestMapping("add")
 	public void add() {
 	}
-	
+
 	@RequestMapping(value = "add", method = RequestMethod.POST)
-	public String add(Model model, ProductDTO dto) throws Exception{
+	public String add(Model model, ProductDTO dto) throws Exception {
 		int result = service.add(dto);
-		
+
 		String path = "commons/message";
-		
-		if(result > 0) {
+
+		if (result > 0) {
 			path = "redirect:/product/list";
 		} else {
 			model.addAttribute("result", "데이터 추가에 실패하였습니다.");
@@ -57,32 +57,42 @@ public class ProductController {
 		}
 		return path;
 	}
-	
+
 	@RequestMapping("delete")
-	public String delete(Model model, ProductDTO dto) throws Exception{
+	public String delete(Model model, ProductDTO dto) throws Exception {
 		int result = service.delete(dto);
-		
+
 		String path = "commons/message";
-		
-		if(result > 0) {
+
+		if (result > 0) {
 			path = "redirect:/product/list";
-		}else {
+		} else {
 			model.addAttribute("result", "데이터 삭제에 실패하였습니다.");
 			model.addAttribute("url", "/product/list");
 		}
-		
+
 		return path;
 	}
-	
+
 	@RequestMapping("update")
-	public void update() throws Exception{
-		
+	public void update(Model model, ProductDTO dto) throws Exception {
+		ProductDTO dto2 = service.getDetail(dto);
+
+		String path = "commons/message";
+		if (dto2 != null) {
+			model.addAttribute("dto", dto2);
+			path = "product/update";
+		} else {
+			model.addAttribute("result", "데이터 삭제에 실패하였습니다.");
+			model.addAttribute("url", "/product/list");
+		}
+
 	}
-	
+
 	@RequestMapping(value = "update", method = RequestMethod.POST)
-	public String update(Model model, ProductDTO dto) throws Exception{
+	public String update2(Model model, ProductDTO dto) throws Exception {
 		int result = service.update(dto);
-		
+
 		return "redirect:/product/list";
 	}
 }
--- src/main/java/com/goodee/app/product/ProductDAO.java
+++ src/main/java/com/goodee/app/product/ProductDAO.java
@@ -1,16 +1,10 @@
 package com.goodee.app.product;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import com.goodee.app.util.DBConnection;
 
 @Repository
 public class ProductDAO {
@@ -23,8 +17,7 @@ public class ProductDAO {
 	
 	public List<ProductDTO> getList() throws Exception{
 		
-		return sqlSession.selectList(NAMESPACE + "getList");
-		
+		return sqlSession.selectList(NAMESPACE + "getList");	
 	}
 	
 	public ProductDTO getDetail(ProductDTO dto) throws Exception{
@@ -33,58 +26,17 @@ public class ProductDAO {
 	}
 	
 	public int add(ProductDTO dto) throws Exception{
-		Connection con = dbConnection.getConnection();
 		
-		String sql = "INSERT INTO PRODUCT_INFO VALUES (PRODUCT_SEQ.NEXTVAL,?,?,?)";
-		
-		PreparedStatement st = con.prepareStatement(sql);
-		
-		st.setString(1, dto.getProduct_name());
-		st.setDouble(2, dto.getProduct_rate());
-		st.setString(3, dto.getProduct_ex());
-		
-		int result = st.executeUpdate();
-		
-		st.close();
-		con.close();
-		
-		return result;
+		return sqlSession.insert(NAMESPACE + "add", dto);
 	}
 	
 	public int delete(ProductDTO dto) throws Exception{
-		Connection con = dbConnection.getConnection();
 		
-		String sql = "DELETE PRODUCT_INFO WHERE PRODUCT_NUM =?";
-		
-		PreparedStatement st = con.prepareStatement(sql);
-		
-		st.setInt(1, dto.getProduct_num());
-		
-		int result = st.executeUpdate();
-		
-		st.close();
-		con.close();
-		
-		return result;
+		return sqlSession.delete(NAMESPACE + "delete", dto);
 	}
 	
 	public int update(ProductDTO dto) throws Exception{
-		Connection con = dbConnection.getConnection();
 		
-		String sql = "UPDATE PRODUCT_INFO SET PRODUCT_NAME =?, PRODUCT_RATE =?, PRODUCT_EX =? "
-				+ " WHERE PRODUCT_NUM = ?";
-		
-		PreparedStatement st = con.prepareStatement(sql);
-		
-		st.setString(1, dto.getProduct_name());
-		st.setDouble(2, dto.getProduct_rate());
-		st.setString(3, dto.getProduct_ex());
-		
-		int result = st.executeUpdate();
-		
-		st.close();
-		con.close();
-		
-		return result;
+		return sqlSession.update(NAMESPACE + "update", dto);
 	}
 }
