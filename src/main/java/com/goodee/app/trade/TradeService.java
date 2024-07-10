c89d7293df0898c7e0f1376883f281a33f208511
j154370 <lihyo15@gmail.com>
2024-07-10 12:30:25 +0900

    aa

--- src/main/java/com/goodee/app/trade/TradeService.java
+++ src/main/java/com/goodee/app/trade/TradeService.java
@@ -3,6 +3,9 @@ package com.goodee.app.trade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.goodee.app.account.AccountDAO;
+import com.goodee.app.account.AccountDTO;
+
 @Service
 public class TradeService {
 
@@ -10,7 +13,19 @@ public class TradeService {
 	@Autowired
 	private TradeDAO tradeDAO;
 	
+	@Autowired
+	private AccountDAO accountDAO;
+	
 	public int trade(TradeDTO tradeDTO) throws Exception{
+		
+		// 잔액 확인해서 입금액보다 적으면 취소하는 코드
+		AccountDTO accountDTO = new AccountDTO();
+		accountDTO.setAccount_num(tradeDTO.getAccount_num());
+		accountDTO = accountDAO.detail(accountDTO);
+		
+		if(accountDTO.getBalance() < tradeDTO.getTrade_amount()) {
+			return 0;
+		}
 		
 		tradeDTO.setTrade_amount(tradeDTO.getTrade_amount() * -1);
 		tradeDTO.setTrade_type("출금");
