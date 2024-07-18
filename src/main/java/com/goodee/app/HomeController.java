daadbc7a4054471d4dc1feab0f4bd13c24ca4d0c
j154370 <lihyo15@gmail.com>
2024-07-18 09:31:53 +0900

    aa

--- src/main/java/com/goodee/app/HomeController.java
+++ src/main/java/com/goodee/app/HomeController.java
@@ -29,8 +29,6 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(HttpServletRequest request) {
-		
-		System.out.println("Study_Conflict");
 
 		// 하위내장객체 -> 상위내장객체 가능
 		// 상위내장객체 -> 하위내장객체 불가능
